fde11545a934e8337a29b67c82ade7ce75cbf0fa
Shevchenko Andrey <demorest49de@gmail.com>
2023-09-19 20:47:10 +0300

    Changes burgerHandler.js changed;

--- src/js/module/shop/burger/burgerHandler.js
+++ src/js/module/shop/burger/burgerHandler.js
@@ -17,8 +17,7 @@ export const burgerHandler = ($) => {
         burgerMenu.style.opacity = '1';
     };
     
-    burgerBtn.addEventListener('click', (ev) => {
-        
+    burgerBtn.addEventListener('click', () => {
         if ($.burgerMenu.visibility) {
             closeMenu();
         } else {
@@ -26,7 +25,6 @@ export const burgerHandler = ($) => {
         }
         $.burgerMenu.visibility = !$.burgerMenu.visibility;
     });
-    
     
     burgerMenu.addEventListener('click', ({target}) => {
         if (target === burgerMenu) {
