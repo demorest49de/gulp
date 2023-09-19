7804a32d941bd8ee1beae8b26448716db5951c89
Shevchenko Andrey <demorest49de@gmail.com>
2023-09-19 18:55:02 +0300

    Changes _burger.scss changed; _header.scss changed; burgerHandler.js changed; burgerHandler.js changed; burgerHandler.js.map changed; constants.js changed; constants.js changed; constants.js.map changed; index.css changed; index.css.map changed;

--- src/js/module/shop/burger/burgerHandler.js
+++ src/js/module/shop/burger/burgerHandler.js
@@ -1,6 +1,7 @@
 export const burgerHandler = ($) => {
     const burgerBtn = document.querySelector('.header__button-menu');
     const menuSvg = document.querySelector('.header__button-menu-svg use');
+    const burgerMenu = document.querySelector('.burger');
     
     burgerBtn.addEventListener('click', (ev) => {
         ev.stopPropagation();
@@ -8,8 +9,14 @@ export const burgerHandler = ($) => {
         console.log(' : ',$.burgerMenu.visibility);
         if($.burgerMenu.visibility){
             menuSvg.setAttribute('href', $.burgerMenu.menu);
+            document.body.style.overflow = 'hidden';
+            burgerMenu.style.opacity = '0';
+            document.body.style.overflow = 'hidden';
         }else{
             menuSvg.setAttribute('href', $.burgerMenu.clear);
+            burgerMenu.style.visibility = 'visible';
+            burgerMenu.style.opacity = '1';
+            document.body.style.overflow = 'hidden';
         }
     });
 };
