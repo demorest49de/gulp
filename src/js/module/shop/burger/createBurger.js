769ac5ab65b3821d27623e31422e4d0eda2276b3
Shevchenko Andrey <demorest49de@gmail.com>
2023-09-19 11:07:28 +0300

    Changes _base.scss changed; _burger.scss added; _global.scss changed; _header.scss changed; _var.scss changed; baseElems.js changed; baseElems.js changed; baseElems.js.map changed; createBurger.js changed; createBurger.js changed; createBurger.js.map changed; index.css changed; index.css.map changed; index.scss changed;

--- src/js/module/shop/burger/createBurger.js
+++ src/js/module/shop/burger/createBurger.js
@@ -2,28 +2,49 @@ export const createBurger = () => {
     const burger = document.createElement('div');
     burger.classList.add('burger');
     burger.insertAdjacentHTML('beforeend', `
-      <div class="burger__container">
-        <nav class="burger__nav">
-          <ul class="burger__list">
-            <li class="burger__item">
-              <a class="burger__link" href="#rooms" >Залы</a>
-            </li>
-            <li class="burger__item">
-              <a class="burger__link" href="#about" >О нас</a>
-            </li>
-            <li class="burger__item">
-              <a class="burger__link" href="#booking" >Бронь</a>
-            </li>
-            <li class="burger__item">
-              <a class="burger__link" href="#reviews" >Отзывы</a>
-            </li>
-            <li class="burger__item">
-              <a class="burger__link" href="#contacts" >Контакты</a>
-            </li>
-          </ul>
-        </nav>
-        <button class="burger__call-button">Заказать звонок</button>
-      </div>
+
+        <div class="burger__menu navbase">
+              <div class="container navbase__container">
+                    <div class="burger__block navbase__block">
+                        <div class="navbase__catalog">
+                            <h2 class="navbase__subtitle">Каталог</h2>
+                            <nav class="navbase__nav">
+                                <ul class="navbase__list">
+                                    <li class="navbase__elem"><a href="#">Смартфоны</a></li>
+                                    <li class="navbase__elem"><a href="#">Ноутбуки</a></li>
+                                    <li class="navbase__elem"><a href="#">Ювелирные изделия</a></li>
+                                    <li class="navbase__elem"><a href="#">Одежда</a></li>
+                                    <li class="navbase__elem"><a href="#">Бытовая техника</a></li>
+                                    <li class="navbase__elem"><a href="#">Бытовая химия</a></li>
+                                    <li class="navbase__elem"><a href="#">Книги и журналы</a></li>
+                                    <li class="navbase__elem"><a href="#">Домашний текстиль</a></li>
+                                    <li class="navbase__elem"><a href="#">Электроника</a></li>
+                                    <li class="navbase__elem"><a href="#">Косметика</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="navbase__buyers-info">
+                            <h2 class="navbase__subtitle">Покупателям</h2>
+                            <nav class="navbase__nav">
+                                <ul class="navbase__list">
+                                    <li class="navbase__elem"><a href="#">Оплата заказа</a></li>
+                                    <li class="navbase__elem"><a href="#">Условия доставки</a></li>
+                                    <li class="navbase__elem"><a href="#">Условия возврата товара</a></li>
+                                    <li class="navbase__elem"><a href="/blog.html">Блог</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="navbase__contacts">
+                            <h2 class="navbase__subtitle">Связаться с нами</h2>
+                            <nav class="navbase__nav">
+                                <ul class="navbase__list">
+                                    <li class="navbase__elem"><a href="#">Контакты</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+              </div>
+        </div>
   `);
     return burger;
 };
