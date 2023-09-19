664c8d27471d093d6d94ef0659914836156c5872
Shevchenko Andrey <demorest49de@gmail.com>
2023-09-19 11:45:45 +0300

    Changes _burger.scss change; createBurger.js change; createBurger.js change; createBurger.js.map change; index.css change; index.css.map change;

--- src/js/module/shop/burger/createBurger.js
+++ src/js/module/shop/burger/createBurger.js
@@ -6,27 +6,27 @@ export const createBurger = () => {
         <div class="burger__menu navbase">
               <div class="container navbase__container">
                     <div class="burger__block navbase__block">
-                        <div class="navbase__catalog">
-                            <h2 class="navbase__subtitle">Каталог</h2>
-                            <nav class="navbase__nav">
-                                <ul class="navbase__list">
-                                    <li class="navbase__elem"><a href="#">Смартфоны</a></li>
-                                    <li class="navbase__elem"><a href="#">Ноутбуки</a></li>
-                                    <li class="navbase__elem"><a href="#">Ювелирные изделия</a></li>
-                                    <li class="navbase__elem"><a href="#">Одежда</a></li>
-                                    <li class="navbase__elem"><a href="#">Бытовая техника</a></li>
-                                    <li class="navbase__elem"><a href="#">Бытовая химия</a></li>
-                                    <li class="navbase__elem"><a href="#">Книги и журналы</a></li>
-                                    <li class="navbase__elem"><a href="#">Домашний текстиль</a></li>
-                                    <li class="navbase__elem"><a href="#">Электроника</a></li>
-                                    <li class="navbase__elem"><a href="#">Косметика</a></li>
+                        <div class="burget__catalog navbase__catalog">
+                            <h2 class="burger__subtitle navbase__subtitle">Каталог</h2>
+                            <nav class="burger__nav navbase__nav">
+                                <ul class="burger__list navbase__list">
+                                    <li class="burger__elem navbase__elem"><a href="#">Смартфоны</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Ноутбуки</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Ювелирные изделия</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Одежда</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Бытовая техника</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Бытовая химия</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Книги и журналы</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Домашний текстиль</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Электроника</a></li>
+                                    <li class="burger__elem navbase__elem"><a href="#">Косметика</a></li>
                                 </ul>
                             </nav>
                         </div>
                         <div class="navbase__buyers-info">
-                            <h2 class="navbase__subtitle">Покупателям</h2>
+                            <h2 class="burger__subtitle navbase__subtitle">Покупателям</h2>
                             <nav class="navbase__nav">
-                                <ul class="navbase__list">
+                                <ul class="burger__list navbase__list">
                                     <li class="navbase__elem"><a href="#">Оплата заказа</a></li>
                                     <li class="navbase__elem"><a href="#">Условия доставки</a></li>
                                     <li class="navbase__elem"><a href="#">Условия возврата товара</a></li>
@@ -34,11 +34,11 @@ export const createBurger = () => {
                                 </ul>
                             </nav>
                         </div>
-                        <div class="navbase__contacts">
-                            <h2 class="navbase__subtitle">Связаться с нами</h2>
+                        <div class="burger__contacts navbase__contacts">
+                            <h2 class="burger__subtitle navbase__subtitle">Связаться с нами</h2>
                             <nav class="navbase__nav">
-                                <ul class="navbase__list">
-                                    <li class="navbase__elem"><a href="#">Контакты</a></li>
+                                <ul class="burger__list navbase__list">
+                                    <li class="burger__elem navbase__elem"><a href="#">Контакты</a></li>
                                 </ul>
                             </nav>
                         </div>
