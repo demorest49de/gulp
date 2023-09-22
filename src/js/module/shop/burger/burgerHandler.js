8efee347fb4d3704035a3ebd78ec78ef3a2f5c2d
Shevchenko Andrey <demorest49de@gmail.com>
2023-09-22 16:17:04 +0300

    Changes _burger.scss changed; burgerHandler.js changed;

--- src/js/module/shop/burger/burgerHandler.js
+++ src/js/module/shop/burger/burgerHandler.js
@@ -4,21 +4,34 @@ export const burgerHandler = ($) => {
     const burger = document.querySelector('.burger');
     const burgerMenu = document.querySelector('.burger__menu');
     
+    // const updateBurgerHeight = () => {
+    //     const height = getComputedStyle(burgerMenu).height;
+    //     console.log(' : ', height);
+    // };
+    
+    const checkWindowsWidth = () => {
+        if (screen.width <= 440) {
+            burgerMenu.style.paddingBottom = '45px';
+        }
+    };
+    
+    
     const closeMenu = () => {
         menuSvg.setAttribute('href', $.burgerMenu.menu);
         document.body.style.overflow = 'visible';
-        burger.style.visibility = 'hidden';
-        burger.style.opacity = '0';
-    
+        // burger.style.visibility = 'hidden';
+        // burger.style.opacity = '0';
+        
         burgerMenu.style.paddingBottom = '0px';
     };
     
     const openMenu = () => {
         menuSvg.setAttribute('href', $.burgerMenu.clear);
         document.body.style.overflow = 'hidden';
-        burger.style.visibility = 'visible';
-        burger.style.opacity = '1';
-    
+        
+        // burger.style.visibility = 'visible';
+        // burger.style.opacity = '1';
+        
         burgerMenu.style.paddingBottom = '92px';
     };
     
@@ -36,5 +49,41 @@ export const burgerHandler = ($) => {
             closeMenu();
             $.burgerMenu.visibility = !$.burgerMenu.visibility;
         }
+    });
+    
+    const burgerMenuWidth = () => {
+        switch (true) {
+            case screen.width <= 1840:
+                burgerMenu.style.height = '700';
+                return;
+            case screen.width <= 1024:
+                burgerMenu.style.height = '460px';
+                return;
+            case screen.width <= 440:
+                burgerMenu.style.height = '680';
+                return;
+            default:
+                burgerMenu.style.height = '500px';
+                return;
+        }
+    };
+
+
+// @media (max-width: 1840px) {
+//         height: 700px;
+//     }
+// @media (max-width: 1024px) {
+//         height: 460px;
+//     }
+// @media (max-width: 440px) {
+//         height: 680px;
+//     }
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        checkWindowsWidth();
+    });
+    
+    window.addEventListener('resize', () => {
+        checkWindowsWidth();
     });
 };
