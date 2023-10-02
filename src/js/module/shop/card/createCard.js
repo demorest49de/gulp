41b1f745a491b9f8249812259551b41dd4d5dece
Shevchenko Andrey <demorest49de@gmail.com>
2023-10-02 13:19:45 +0300

    Changes _card.scss _category.scss createCard.js createCard.js index.min.css index.min.css.map

--- src/js/module/shop/card/createCard.js
+++ src/js/module/shop/card/createCard.js
@@ -4,20 +4,45 @@ export const createSectionCard = (name, $, paramsObject) => {
     
     const cardId = paramsObject.id;
     getItemById($, cardId).then((data) => {
-        console.log(' : ',data.data);
+        console.log(' : ', data.data);
         const item = data.data;
-        
-            $.main.insertAdjacentHTML('beforeend',
-                `
+        $.main.insertAdjacentHTML('beforeend',
+            `
             <section class="${name}" aria-label="${item.title}">
             <h2 class="visually-hidden">${item.title}</h2>
             <div class="container">
                 <h3 class="card__title">${item.title}</h3>
                 <div class="card__block">
-                
+                    <picture class="card__figure">
+                    <img loading="lazy" class="card__image"
+                     src="${$.URL}/${item.image}"
+                     alt="${item.title}" width="757" height="427">
+                    </picture>
                 </div>
             </div>
         </section>
             `);
+        const cardBlock = $.main.querySelector('.card__block');
+        cardBlock.style.width = '757px';
+        cardBlock.style.height = '427px';
+        
+        const cardFigure = $.main.querySelector('.card__figure');
+        cardFigure.style.height = '100%';
+        
+        const cardTitle = $.main.querySelector('.card__title');
+        cardTitle.style.marginBottom = '69px';
+    
+        const cardAfterStyle = document.createElement("style");
+        if (item.discount > 0) {
+            cardAfterStyle.innerHTML +=
+                `.card__figure:after {
+                content: '-${item.discount}%';
+                top: 20px;
+                right: 20px;
+                bottom: unset;
+                left: unset;
+            }`;
+            document.body.append(cardAfterStyle);
+        }
     });
 };
