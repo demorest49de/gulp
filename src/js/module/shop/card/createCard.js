4f6dea3e3b9abe5cf5e396f8425f1696abd79e53
Shevchenko Andrey <demorest49de@gmail.com>
2023-10-03 09:42:57 +0300

    Changes _details.scss createCard.js

--- src/js/module/shop/card/createCard.js
+++ src/js/module/shop/card/createCard.js
@@ -63,6 +63,34 @@ export const createSectionCard = (name, $, paramsObject) => {
                             </div>
                         </div>
                         <p class="details__credit-from">В кредит от ${creditfrom} ₽</p>
+                        <div class="details__add-block">
+                            <button class="details__add-to-card">Добавить в корзину</button>
+                            <button class="details__add-to-favorite">
+                                    <svg class="details__add-to-favorite-icon" xmlns="http://www.w3.org/2000/svg"
+                                     width="33" height="33" viewBox="0 0 33 33" >
+                                        <path d="M22.6875 4.125C20.295 4.125 17.9987
+                                        5.23875 16.5 6.99875C15.0012 5.23875 12.705 4.125
+                                        10.3125 4.125C6.0775 4.125 2.75 7.4525 2.75 11.6875C2.75
+                                        16.885 7.425 21.12 14.5062 27.555L16.5 29.3563L18.4937
+                                         27.5413C25.575 21.12 30.25 16.885 30.25
+                                         11.6875C30.25 7.4525 26.9225 4.125 22.6875
+                                          4.125ZM16.6375 25.5062L16.5 25.6437L16.3625
+                                          25.5062C9.8175 19.58 5.5 15.6613 5.5 11.6875C5.5
+                                          8.9375 7.5625 6.875 10.3125 6.875C12.43 6.875 14.4925
+                                          8.23625 15.2212 10.12H17.7925C18.5075 8.23625 20.57
+                                          6.875 22.6875 6.875C25.4375 6.875 27.5 8.9375 27.5
+                                           11.6875C27.5 15.6613 23.1825 19.58 16.6375 25.5062Z"/>
+                                    </svg>
+
+                            </button>
+                        </div>
+                        <div class="details__info-block">
+                            <p class="details__text-info">Доставка</p>
+                            <p class="details__text-details">1-3 января</p>
+                            <p class="details__text-info">Продавец</p>
+                            <p class="details__text-details">ShopOnline</p>
+                        </div>
+                            <button class="details__warn-about-price-reduction">Узнать о снижении цены</button>
                     </div>
                 </div>
             </div>
