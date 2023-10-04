2830d49e9210b9c539d8e53892cb23cc94a14dbf
Shevchenko Andrey <demorest49de@gmail.com>
2023-10-04 14:10:26 +0300

    Changes _basket.scss createBasket.js createBasket.js index.min.css index.min.css.map

--- src/js/module/shop/basket/createBasket.js
+++ src/js/module/shop/basket/createBasket.js
@@ -83,15 +83,23 @@ export const createSectionBasket = (name, $) => {
                                  src="" alt="" width="" height="">
                         </picture>
                     </div>
-                    <div class="basket__list-text-block">
-                        <p class="basket__list-text-main">
-                            15.6" Игровой ноутбук ASUS G513IE-HN004T</p>
-                        <p class="basket__list-text-additional">
-                            Цвет: черный
-                        </p>
-                        <p class="basket__list-text-additional">
-                            Оперативная память: 16 ГБ
-                        </p>
+                    <div class="basket__list-all-info-block">
+                        <div class="basket__list-text-block">
+                            <p class="basket__list-text-main">
+                                15.6" Игровой ноутбук ASUS G513IE-HN004T</p>
+                            <p class="basket__list-text-additional">
+                                Цвет: черный
+                            </p>
+                            <p class="basket__list-text-additional">
+                                Оперативная память: 16 ГБ
+                            </p>
+                        </div>
+                        <div class="basket__list-quantity-block">
+                        1
+                        </div>
+                        <div class="basket__list-price-block">
+                        1
+                        </div>
                     </div>
                 </li>
                 <li class="basket__list-item">
